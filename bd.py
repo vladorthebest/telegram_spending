b34e979283c95e814b2599a7fa3c5398cef402a1
vladorthebest <vladorthebest@gmail.com>
2022-03-30 19:08:32 +0200

    add comments and fix new record

--- bd.py
+++ bd.py
@@ -2,33 +2,45 @@ import sqlite3
 from datetime import datetime 
 
 class Data_Base():
-    con = sqlite3.connect('data_base.db', check_same_thread=False)
-    cursor = con.cursor()
+    con = sqlite3.connect('data_base.db', check_same_thread=False)  #connect DataBase
+    cursor = con.cursor() #create cursor for DataBase
 
     def __init__(self):
-
+        """ Create table in the DataBase """
+        
         Data_Base.cursor.execute('''CREATE TABLE IF NOT EXISTS expenses
-                            (user_id text, data text, type text, cost float)''') 
+                            (user_id text, data text, type text, cost float)''')  
         
     
     def add_in_bd(self, cost, type, user_id):
+        """ Add new record in the DataBase
+        Args: value, category, user_id
+        """
 
-
+        #command "adding"
         query = '''INSERT INTO expenses VALUES 
                         (?, ?, ?, ?)'''
         
-        today = str(datetime.now())
-        Data_Base.cursor.execute(query, (user_id, today, type, cost))
-        Data_Base.con.commit()
+        today = str(datetime.now()) #date today
 
+        #add record in DataBase
+        Data_Base.cursor.execute(query, (user_id, today, type, cost)) 
+        Data_Base.con.commit()
+        
+        print(f"Add {user_id}: {type} {cost}") #log
 
     def get_all_cost(self, typ, user_id):
-        expenses = 0
+        """ Function return cost amount from the category
+        Args: category, user_id
+        """
+        expenses = 0 #cost amount
 
+        #select all record in the category from user
         for cost in Data_Base.cursor.execute(f''' SELECT cost FROM expenses WHERE type = "{typ}" AND user_id = "{user_id}"'''):
-            print(cost)
-            expenses += float(cost[0])
-        
+            expenses += float(cost[0]) #add cost in the cost amount
+
+        print(f"Result {user_id}: {typ} {expenses}") #log
+
         return expenses
 
             
--- bot.py
+++ bot.py
@@ -10,41 +10,47 @@ menu_keyboard.row('Продукты')
 menu_keyboard.row('Подарок')
 menu_keyboard.row('Путешествие')
 
+#start bot
 @bot.message_handler(commands='start')
 def main(message):
     bot.send_message(message.chat.id,
     """Я бот капиталист\n
 Посчитаю твои убытки, а может заработки\n
 Хотя зная тебя я буду считать рассходы)))""",
-    reply_markup=menu_keyboard
+    reply_markup=menu_keyboard  #add keyboard
     )
 
 
+#command 'result' - sends value your bank account category
 @bot.message_handler(commands="result")
 def ask(message):
-    bot.send_message(message.chat.id, "Какой счет интересует?", reply_markup=menu_keyboard)
-    bot.register_next_step_handler(message, get_bd)
+    bot.send_message(message.chat.id, "Какой счет интересует?", reply_markup=menu_keyboard) #input category
+    bot.register_next_step_handler(message, get_bd) #next step
 
 def get_bd(message):
-    all_spending = base.get_all_cost(message.text, message.chat.id)
-    bot.send_message(message.chat.id, f"{message.text} : {all_spending}")
+    all_spending = base.get_all_cost(message.text, message.chat.id) #get value in the DataBase
+    bot.send_message(message.chat.id, f"{message.text} : {all_spending}") #send value in the user
 
 
+#text - add new record in the DataBase
 @bot.message_handler(content_types=['text'])
 def ask(message):
-    typ = message.text
-    bot.register_next_step_handler(message, plus, typ)
+    message_typ = message #input category record
+    bot.register_next_step_handler(message, plus, message_typ)
+    
 
-def plus(message, typ):
+def plus(message, message_typ):
     try:
-        lost = float(message.text)
-        print(typ, lost)
-        base.add_in_bd(lost, typ, message.chat.id)
-        bot.send_message(message.chat.id, f"Запись добавлена!\n{typ}:{lost}")
+        lost = float(message.text) #user result
+        
+        base.add_in_bd(lost, message_typ.text, message.chat.id) #add record in the DataBase
+        bot.send_message(message.chat.id, f"Запись добавлена!\n{message_typ.text}:{lost}") #send alert about add record for user 
 
+
+    #if user result doesn`t float/int
     except:
-        bot.send_message(message.chat.id, "Цифры вводить нужно!")
-        bot.register_next_step_handler(message, typ, plus, typ)
+        bot.send_message(message.chat.id, "Цифры вводить нужно!") 
+        bot.register_next_step_handler(message, plus, message_typ) #again start this function
      
    
 
